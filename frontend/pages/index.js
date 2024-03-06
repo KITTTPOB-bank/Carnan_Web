c699d52eab9c3875a31d8492bd2be41e3c1054ca
KITTTPOB-bank <160011796+KITTTPOB-bank@users.noreply.github.com>
2024-03-06 17:44:30 +0700

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
 
   const fetchDatacar = async () => {
     try {
-      const apiUrl = 'http://3.81.234.197/getallcar';
+      const apiUrl = 'https://3.81.234.197/getallcar';
       const response = await axios.get(apiUrl);
       const data = response.data
       const yearArray = [];
